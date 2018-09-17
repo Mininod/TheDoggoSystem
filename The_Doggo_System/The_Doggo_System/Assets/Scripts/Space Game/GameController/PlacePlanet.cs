7387d535b0bd07db3e97e8f307fc1e57f009ab01
abelitt <abelitt@hotmail.co.uk>
2018-09-17 20:00:40 +0100

    LATEST
    Arrow direction fixed. Now needs to be attached to planet and then I should be good to start the velocity of the planets

--- The_Doggo_System/The_Doggo_System/Assets/Scripts/Space Game/GameController/PlacePlanet.cs
+++ The_Doggo_System/The_Doggo_System/Assets/Scripts/Space Game/GameController/PlacePlanet.cs
@@ -23,9 +23,12 @@ public class PlacePlanet : MonoBehaviour {
 
     private GameObject UFO;
     private float distance;
-    private float SCALEMULTIPLYER = 500;
+    private float SCALEMULTIPLYER = 125;
+
+    public GameObject velocityArrow;
 
     protected bool editMode = false;
+    protected bool velocityMode = false;
 
     // Use this for initialization
     void Start () {
@@ -46,7 +49,7 @@ public class PlacePlanet : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.touchCount == 2) 
+        if (Input.touchCount == 2 && velocityMode == false) 
         {
             //PAUSE
             if (Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetTouch(1).phase == TouchPhase.Ended)
@@ -62,7 +65,6 @@ public class PlacePlanet : MonoBehaviour {
                     tapLocation = cam.GetComponent<Camera>().ScreenToWorldPoint(new Vector3((Input.GetTouch(0).position.x + Input.GetTouch(1).position.x) / 2, (Input.GetTouch(0).position.y + Input.GetTouch(1).position.y) / 2, 1));
                     UFO = Instantiate(planet, tapLocation, planet.transform.rotation);
                     editMode = true;
-                    UFO.GetComponent<Button>().onClick.AddListener(PlanetClick); 
                 }
                   
             }
@@ -71,7 +73,7 @@ public class PlacePlanet : MonoBehaviour {
             {
                 
                 distance = Mathf.Sqrt(((Input.GetTouch(0).position.x - Input.GetTouch(1).position.x) * (Input.GetTouch(0).position.y - Input.GetTouch(1).position.y)) + ((Input.GetTouch(0).position.y - Input.GetTouch(1).position.y) * (Input.GetTouch(0).position.y - Input.GetTouch(1).position.y))); //The distance between both fingers 
-                debugText.text = distance.ToString();
+                //debugText.text = distance.ToString();
                 UFO.GetComponent<Transform>().localScale = new Vector3(distance / SCALEMULTIPLYER, distance / SCALEMULTIPLYER, 1);
             }
 
@@ -80,27 +82,42 @@ public class PlacePlanet : MonoBehaviour {
            
         }
 
+        if (Input.touchCount == 1 && velocityMode == true)
+        {
+            //Velocity mode On, start
+            //float fingerToArrowPosition = Vector2.Angle(Input.GetTouch(0).position, UFO.transform.position);
+            Vector2 inputWorldPoint = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            float angle = Mathf.Atan2(inputWorldPoint.y - UFO.transform.position.y, inputWorldPoint.x - UFO.transform.position.x) * 180 / Mathf.PI;
+            debugText.text = angle.ToString();
+            velocityArrow.transform.eulerAngles = new Vector3(0, 0, angle+90);
+        }
+
     }
     
-    void PlanetClick()
-    {
-        
-       
-        
-    }
-
+    
     void VelocityButtonOnClick()
     {
         velocityButtonGameObject.SetActive(false);
-
+        velocityMode = true;
+        
 
     }
 
     void TickButtonOnClick()
     {
-        tickButtonGameObject.SetActive(false);
-        velocityButtonGameObject.SetActive(false);
-        editMode = false;
+        if (velocityMode == true) //If editing velocity
+        {
+            velocityMode = false;                               //Out of velocity
+            velocityButtonGameObject.SetActive(true);           //Re-see the velocity
+            
+        }
+        else
+        {
+            tickButtonGameObject.SetActive(false);          //
+            velocityButtonGameObject.SetActive(false);      //  Out of Edit mode
+            editMode = false;                               //
+        }
+        
     }
 
 
