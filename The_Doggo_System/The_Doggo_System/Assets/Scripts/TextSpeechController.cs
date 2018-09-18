ca65334b34f018af448554802656d3115538c56b
Mininod <35764343+Mininod@users.noreply.github.com>
2018-09-18 16:33:02 +0100

    text and photoshop fles

--- The_Doggo_System/The_Doggo_System/Assets/Scripts/TextSpeechController.cs
+++ The_Doggo_System/The_Doggo_System/Assets/Scripts/TextSpeechController.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class TextSpeechController : MonoBehaviour {
 
+    public Image textDisplay;
     public Text outputTextBox;
     public int typeSpeed; //Lower is faster
     public string[] inputText;
@@ -14,9 +15,11 @@ public class TextSpeechController : MonoBehaviour {
     private int m_currentLetter;
     private bool m_isTextDialogueComplete;
     private bool m_isWaitingForTextLine;
+    private bool m_isTextBoxOnScreen;
 
 	// Use this for initialization
 	void Start () {
+        m_isTextBoxOnScreen = false;
         m_currentLine = 0;
         m_isTextDialogueComplete = false;
         m_maxTypeSpeed = typeSpeed;
@@ -25,35 +28,45 @@ public class TextSpeechController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-        if (!m_isTextDialogueComplete)
+        if (!m_isTextBoxOnScreen && !m_isTextDialogueComplete)
         {
-            if (!m_isWaitingForTextLine)
+            MoveTextBoxDownOrUp();
+        }
+        else
+        {
+            if (!m_isTextDialogueComplete)
             {
-                if (typeSpeed <= 0)
+                if (!m_isWaitingForTextLine)
                 {
-                    typeSpeed = m_maxTypeSpeed;
-                    if (m_currentLine < inputText.Length && m_currentLetter < inputText[m_currentLine].Length)
+                    if (typeSpeed <= 0)
                     {
-                        outputTextBox.text += inputText[m_currentLine][m_currentLetter];
-                        m_currentLetter++;
-                    }
-                    else if (m_currentLine < inputText.Length)
-                    {
-                        m_currentLine++;
-                        m_currentLetter = 0;
-                        m_isWaitingForTextLine = true;
+                        typeSpeed = m_maxTypeSpeed;
+                        if (m_currentLine < inputText.Length && m_currentLetter < inputText[m_currentLine].Length)
+                        {
+                            outputTextBox.text += inputText[m_currentLine][m_currentLetter];
+                            m_currentLetter++;
+                        }
+                        else if (m_currentLine < inputText.Length)
+                        {
+                            m_currentLine++;
+                            m_currentLetter = 0;
+                            m_isWaitingForTextLine = true;
+                        }
+                        else
+                        {
+                            outputTextBox.text = "";
+                            m_isTextDialogueComplete = true; 
+                        }
                     }
                     else
                     {
-                        outputTextBox.text = "";
-                        m_isTextDialogueComplete = true;
+                        typeSpeed--;
                     }
                 }
-                else
-                {
-                    typeSpeed--;
-                }
+            }
+            else if(m_isTextBoxOnScreen)
+            {
+                MoveTextBoxDownOrUp();
             }
         }
 
@@ -70,6 +83,32 @@ public class TextSpeechController : MonoBehaviour {
         {
             m_isWaitingForTextLine = false;
             outputTextBox.text = "";
+        }
+    }
+
+    void MoveTextBoxDownOrUp()
+    {
+        if (!m_isTextBoxOnScreen)
+        {
+            if (textDisplay.GetComponent<RectTransform>().anchoredPosition.y > -66)
+            {
+                textDisplay.GetComponent<RectTransform>().position -= new Vector3(0, 3, 0);
+            }
+            else
+            {
+                m_isTextBoxOnScreen = true;
+            }
+        }
+        else
+        {
+            if (textDisplay.GetComponent<RectTransform>().anchoredPosition.y < 66)
+            {
+                textDisplay.GetComponent<RectTransform>().position += new Vector3(0, 3, 0);
+            }
+            else
+            {
+                m_isTextBoxOnScreen = false;
+            }
         }
     }
 }
