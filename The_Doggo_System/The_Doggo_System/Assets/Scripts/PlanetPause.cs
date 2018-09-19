68df38ba362eea5bb51c65e671e10135071c7cd7
Mininod <35764343+Mininod@users.noreply.github.com>
2018-09-19 13:52:09 +0100

    Pause Added

---
+++ The_Doggo_System/The_Doggo_System/Assets/Scripts/PlanetPause.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetPause : MonoBehaviour {
+
+    public bool isGamePaused;
+
+    private bool m_isObjCurrentlyPaused;
+
+    private Vector3 m_currentVelocity;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isGamePaused)
+        {
+            if(!m_isObjCurrentlyPaused)
+            {
+                m_currentVelocity = gameObject.GetComponent<Rigidbody2D>().velocity;
+                gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+                m_isObjCurrentlyPaused = true;
+            }
+        }
+        else if(m_isObjCurrentlyPaused && !isGamePaused)
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = m_currentVelocity;
+            m_isObjCurrentlyPaused = false;
+        }
+	}
+}
